515bf21830ea8a0781978c5e620877602ef6700f
Fabeliz Alvarez <fabelizfi1@gmail.com>
2024-09-20 11:45:33 -0400

    actualizacion

--- components/admin/AdminRoute.tsx
+++ components/admin/AdminRoute.tsx
@@ -24,6 +24,6 @@ console.log("isActive: ", isActive)
       className={`${isActive ? 'bg-amber-400' : ''} font-bold text-lg border-t border-gray-200 p-3 last-of-type:border-b`}
       href={link.url}
       target={link.blank ? '_blank' : ''}
-    >{link.text} - {"isActive: " + isActive}</Link>
+    >{link.text}</Link>
   )
 }
